bda71a4624340bb2a9b0bcf541b16bc11cb522c1
leesheetrit <67081991+leesheetrit@users.noreply.github.com>
2023-06-19 16:14:13 -0400

    Update TMT_CustomTkinter.py

--- TMT_CustomTkinter.py
+++ TMT_CustomTkinter.py
@@ -3,9 +3,6 @@
 
 
 #approach:
-    #last working file: TMT_customTkinter before file split
-    #get field mapping window loaded, no button functionality
-    #move to seperate files <- here, getting into name error
     #keep building
     #build functionality for base tool as needed
 
@@ -24,19 +21,42 @@ class NavigationFrame(customtkinter.CTkFrame):
         widgets.append(customtkinter.CTkLabel(self, text="Navigation Window"))
         #Buttons
         widgets.append(customtkinter.CTkButton(self, text="Read Excel TMT File", \
-                                               command=self.read_excel_TMT))
+                                               command=self.master.read_excel_TMT))
         widgets.append(customtkinter.CTkButton(self, text="Map Tape Fields", \
-                                               command=self.open_field_mapping_frame))
+                                               command=self.master.open_field_mapping_frame))
         widgets.append(customtkinter.CTkButton(self, text="Map Tape Values"))
         
         for i, widget in enumerate(widgets):
             widget.grid(row=i, column=0, padx=10, pady=5, sticky="ew")
 
+        
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Tape Mapping Tool")
+        self.geometry("700x700x5x5")
+        self.grid_rowconfigure(0, weight=1)  # configure grid system
+        self.grid_columnconfigure(1,weight = 1) 
+        self.navigation_frame  = NavigationFrame(master=self)
+        self.navigation_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        
+        self.work_bench_frame = customtkinter.CTkFrame(master=self)
+        self.work_bench_frame.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
+        
+        # self.work_buttons_frame = customtkinter.CTkFrame(master=self)
+        # self.work_buttons_frame.grid(row=2, column=1, padx=20, pady=20, sticky="nsew")
+        
+        self.source_columns = {}
+        self.destination_columns = {}
+        self.source_columns_sorted = []
+        self.column_mappings_rules = []
+    
     def read_excel_TMT(self):
         print('Reading Excel TMT...')
-        #call loading screen class
+        #to be: call loading screen class
         print('Excel TMT Read')
-        #call excel loaded class
+        #to be: call excel loaded class
         
         print('Loading Excel File...')
         self.wb_path = 'I:/ServicingMSR/QRM/Trades Upload Files/Tape Mapping Tool/Tape Mapping Tool.xlsx'
@@ -48,8 +68,6 @@ class NavigationFrame(customtkinter.CTkFrame):
         #Find the last used row on the Excel sheet
         print('Finding Last Row on Tape File...')
         self.last_row = self.tape_sheet.max_row
-        self.source_columns = {}
-        self.destination_columns = {}
         
         ####Read Source Columns 
         # Create a dictionary to store the source columns
@@ -60,7 +78,8 @@ class NavigationFrame(customtkinter.CTkFrame):
                 self.source_columns[cell.column_letter] = cell.value
         print('Source Columns Read.')          
         # Sort the source column names alphabetically
-        self.source_column_names = sorted(map(str, self.source_columns.values()))
+        self.source_columns_sorted = sorted(map(str, self.source_columns.values()))
+        # print(type(self.source_column_names))
         print('Source Columns Sorted.')          
 
         ####Read Destination Columns 
@@ -81,25 +100,20 @@ class NavigationFrame(customtkinter.CTkFrame):
                    'dest_value_example': dest_value_example,
                    'is_percentage': is_percentage,
                    }
+        print('Excel TMT has been Loaded')
         
     def open_field_mapping_frame(self):
         print('OpenFieldMappingFrame Method Activated')
-        self.FieldMappingFrame= FieldMappingFrame(master=app)
-        self.FieldMappingFrame.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
-
+        self.work_bench_frame= FieldMappingFrame(master=self,\
+                                                 destination_columns=self.destination_columns, \
+                                                     source_columns =  self.source_columns_sorted )
+        self.work_bench_frame.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
         
-class App(customtkinter.CTk):
-    def __init__(self):
-        super().__init__()
-
-        self.title("Tape Mapping Tool")
-        self.geometry("700x700x5x5")
-        self.grid_rowconfigure(0, weight=1)  # configure grid system
-        self.grid_columnconfigure(1,weight = 1) 
-        self.navigation_frame  = NavigationFrame(master=self)
-        self.navigation_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        #thinking about a buttons frame at the bottom
+        # self.work_buttons_frame = customtkinter.CTkFrame(master=self)
+        # self.work_buttons_frame.grid(row=2, column=1, padx=20, pady=20, height = 10, sticky="sew")
         
-      
+        self.column_mappings_rules = self.work_bench_frame.get_field_mappings()
 
 if __name__ == "__main__":
     customtkinter.set_appearance_mode("dark")
