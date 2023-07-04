e086042fa2a41684c1ca229d3be9ca874846b3fa
leesheetrit <67081991+leesheetrit@users.noreply.github.com>
2023-07-04 14:18:02 -0400

    Update Playing with Custom Tkinter.py

--- Playing with Custom Tkinter.py
+++ Playing with Custom Tkinter.py
@@ -26,6 +26,9 @@ class ValueMappingFrame(customtkinter.CTkScrollableFrame):
         ...
         if self.smart_match:
             for past_destination, past_source in self.past_mappings:
-                if past_destination == item and past_source in self.source_columns:
-                    self.combobox_var.set(past_source)
+                if past_source == unique_source_value1 /
+                and past_destination in self.dest_field_values: #change me!
+                    self.combobox_var.set(past_destination)
                     break
+                    
+                    
