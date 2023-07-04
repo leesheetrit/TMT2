c2a21e5fdf97421b1663d78ea17e2039742209f0
leesheetrit <67081991+leesheetrit@users.noreply.github.com>
2023-07-04 14:11:57 -0400

    Update Playing with Custom Tkinter.py

--- Playing with Custom Tkinter.py
+++ Playing with Custom Tkinter.py
@@ -14,18 +14,18 @@ class ValueMappingFrame(customtkinter.CTkScrollableFrame):
             
         ...
     def query_past_mappings(self):
-        self.query = "SELECT Destination_Field, Source_Field FROM Servicing.dbo.TMT_Field_Mappings"
+        self.query = "SELECT Destination_Value, Source_Value FROM Servicing.dbo.TMT_Value_Mappings"
         self.query_result = self.qrm_db.connect().execute(self.query)
-        self.past_mappings = [(row['Destination_Field'], row['Source_Field']) for row in self.query_result]
+        self.past_mappings = [(row['Destination_Value'], row['Source_Value']) for row in self.query_result]
         
         print("Past Mappings", self.past_mappings)
 
         
 
-
-
-if self.smart_match:
-    for past_destination, past_source in self.past_mappings:
-        if past_destination == item and past_source in self.source_columns:
-            self.combobox_var.set(past_source)
-            break
+    def gen_widgets(self):
+        ...
+        if self.smart_match:
+            for past_destination, past_source in self.past_mappings:
+                if past_destination == item and past_source in self.source_columns:
+                    self.combobox_var.set(past_source)
+                    break
