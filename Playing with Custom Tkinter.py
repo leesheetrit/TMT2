6ccaf5717421ff36d0c9043616763bae2ce3de66
leesheetrit <67081991+leesheetrit@users.noreply.github.com>
2023-07-04 14:09:06 -0400

    Update Playing with Custom Tkinter.py

--- Playing with Custom Tkinter.py
+++ Playing with Custom Tkinter.py
@@ -1,4 +1,26 @@
 
+class ValueMappingFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, ..., smart_match = False, qrm_db = '',**kwargs):
+        super().__init__(master, **kwargs)
+        
+        #Attributes
+     ...
+        self.smart_match = smart_match
+        self.qrm_db = qrm_db
+        
+        #Call Methods
+        if self.smart_match:
+            self.query_past_mappings()
+            
+        ...
+    def query_past_mappings(self):
+        self.query = "SELECT Destination_Field, Source_Field FROM Servicing.dbo.TMT_Field_Mappings"
+        self.query_result = self.qrm_db.connect().execute(self.query)
+        self.past_mappings = [(row['Destination_Field'], row['Source_Field']) for row in self.query_result]
+        
+        print("Past Mappings", self.past_mappings)
+
+        
 
 
 
